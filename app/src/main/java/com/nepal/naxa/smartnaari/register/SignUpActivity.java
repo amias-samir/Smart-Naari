9f02ebdfebfcbba3166841d43de6cfa0560551bc
Samir Dangal <Samir Dangal>
2017-09-06 15:37:17 +0545

    Mitho login page XML done

--- app/src/main/java/com/nepal/naxa/smartnaari/register/SignUpActivity.java
+++ app/src/main/java/com/nepal/naxa/smartnaari/register/SignUpActivity.java
@@ -4,20 +4,29 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 
 import com.nepal.naxa.smartnaari.R;
 import com.nepal.naxa.smartnaari.homescreen.MainActivity;
+import com.nepal.naxa.smartnaari.login.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignUpActivity extends Activity {
+
+    @BindView(R.id.btnSignUp)
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
 
 
-        startActivity(new Intent(this, MainActivity.class));
 
     }
 
@@ -27,7 +36,7 @@ public class SignUpActivity extends Activity {
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.radio_sex_male:
                 if (checked)
                     // Pirates are the best
@@ -37,5 +46,10 @@ public class SignUpActivity extends Activity {
                     // Ninjas rule
                     break;
         }
+    }
+
+    @OnClick(R.id.btnSignUp)
+    public void SignUpBtnClicked() {
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
