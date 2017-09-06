4019a9ffe792c7a54dfe8324e354a24d21281612
Samir Dangal <Samir Dangal>
2017-09-06 11:26:11 +0545

    mitho signup page XML

---
+++ app/src/main/java/com/nepal/naxa/smartnaari/register/SignUpActivity.java
@@ -0,0 +1,35 @@
+package com.nepal.naxa.smartnaari.register;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+import com.nepal.naxa.smartnaari.R;
+
+public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_sex_male:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.radio_sex_female:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
+}
