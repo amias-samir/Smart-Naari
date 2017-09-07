7254df6c85aadf0cf1c5401d3df46952c15188e1
Samir Dangal <smdangal201@gmail.com>
2017-09-07 13:18:33 +0545

    mitho MyCircle page under construction

---
+++ app/src/main/java/com/nepal/naxa/smartnaari/mycircle/MyCircleActivity.java
@@ -0,0 +1,15 @@
+package com.nepal.naxa.smartnaari.mycircle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.nepal.naxa.smartnaari.R;
+
+public class MyCircleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_circle);
+    }
+}
