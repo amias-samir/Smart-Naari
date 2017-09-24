f04caaee187bf7cd6d5f576680c025103efd8148
Susan <susan.invents@gmail.com>
2017-09-24 15:04:06 +0545

    sign up success

--- app/src/main/java/com/nepal/naxa/smartnaari/data/network/UrlClass.java
+++ app/src/main/java/com/nepal/naxa/smartnaari/data/network/UrlClass.java
@@ -6,7 +6,7 @@ package com.nepal.naxa.smartnaari.data.network;
 
 public class UrlClass {
 
-    public static final String BASE_URL = "http://naxa.com.np/smartnaari";
-    public static final String SIGNUP_URL = BASE_URL + "/Smartapi/register";
+    public static final String BASE_URL = "http://naxa.com.np/smartnaari/";
+    public static final String SIGNUP_URL = BASE_URL + "Smartapi/register";
 
 }
