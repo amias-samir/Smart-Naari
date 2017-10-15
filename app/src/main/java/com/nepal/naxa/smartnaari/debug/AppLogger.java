f2ce24ce3ac813acab2a7ad78899dba7e2e3cb00
Nishon Tandukar <nishon.tan@gmail.com>
2017-10-15 15:12:11 +0545

    AppDataManager Last Sync Date Time Completed

---
+++ app/src/main/java/com/nepal/naxa/smartnaari/debug/AppLogger.java
@@ -0,0 +1,44 @@
+package com.nepal.naxa.smartnaari.debug;
+
+import com.nepal.naxa.smartnaari.BuildConfig;
+
+import timber.log.Timber;
+
+/**
+ * Created on 10/15/17
+ * by nishon.tan@gmail.com
+ */
+
+public class AppLogger {
+
+    public static void init() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+
+    public static void d(String s, Object... objects) {
+        Timber.d(s, objects);
+    }
+
+    public static void d(Throwable throwable, String s, Object... objects) {
+        Timber.d(throwable, s, objects);
+    }
+
+    public static void i(String s, Object... objects) {
+        Timber.i(s, objects);
+    }
+
+    public static void i(Throwable throwable, String s, Object... objects) {
+        Timber.i(throwable, s, objects);
+    }
+
+    public static void e(String s, Object... objects) {
+        Timber.e(s, objects);
+    }
+
+    public static void e(Throwable throwable, String s, Object... objects) {
+        Timber.e(throwable, s, objects);
+    }
+
+}
