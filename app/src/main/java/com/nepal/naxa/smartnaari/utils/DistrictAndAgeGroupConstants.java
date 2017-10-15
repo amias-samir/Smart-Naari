471795893da6472f6ea048f8b14dc363eb23c297
Samir Dangal <smdangal201@gmail.com>
2017-10-15 14:50:49 +0545

    signup issue fixed, select district option added to current and birth district spnner

--- app/src/main/java/com/nepal/naxa/smartnaari/utils/DistrictAndAgeGroupConstants.java
+++ app/src/main/java/com/nepal/naxa/smartnaari/utils/DistrictAndAgeGroupConstants.java
@@ -6,7 +6,7 @@ package com.nepal.naxa.smartnaari.utils;
 
 public class DistrictAndAgeGroupConstants {
 
-    public final static String[] districtListEnglish = {"        ","Achham", "Arghakhanchi", "Baglung", "Baitadi", "Bajhang", "Bajura", "Banke", "Bara", "Bardiya", "Bhaktapur",
+    public final static String[] birthDistrictListEnglish = {"Birth District","Achham", "Arghakhanchi", "Baglung", "Baitadi", "Bajhang", "Bajura", "Banke", "Bara", "Bardiya", "Bhaktapur",
             "Bhojpur", "Chitwan", "Dadeldhura", "Dailekh", "Dang", "Darchula", "Dhading", "Dhankuta", "Dhanusa", "Dholkha",
             "Dolpa", "Doti", "Gorkha", "Gulmi", "Humla", "Ilam", "Jajarkot", "Jhapa", "Jumla", "Kailali", "Kalikot", "Kanchanpur",
             "Kapilvastu", "Kaski", "Kathmandu", "Kavrepalanchok", "Khotang", "Lalitpur", "Lamjung", "Mahottari", "Makwanpur",
@@ -14,5 +14,15 @@ public class DistrictAndAgeGroupConstants {
             "Parbat", "Parsa", "Pyuthan", "Ramechhap", "Rasuwa", "Rautahat", "Rolpa", "Rukum", "Rupandehi", "Salyan", "Sankhuwasabha",
             "Saptari", "Sarlahi", "Sindhuli", "Sindhupalchok", "Siraha", "Solukhumbu", "Sunsari", "Surkhet", "Syangja", "Tanahu",
             "Taplejung", "Terhathum", "Udayapur"};
+
+    public final static String[] currentDistrictListEnglish = {"Current District","Achham", "Arghakhanchi", "Baglung", "Baitadi", "Bajhang", "Bajura", "Banke", "Bara", "Bardiya", "Bhaktapur",
+            "Bhojpur", "Chitwan", "Dadeldhura", "Dailekh", "Dang", "Darchula", "Dhading", "Dhankuta", "Dhanusa", "Dholkha",
+            "Dolpa", "Doti", "Gorkha", "Gulmi", "Humla", "Ilam", "Jajarkot", "Jhapa", "Jumla", "Kailali", "Kalikot", "Kanchanpur",
+            "Kapilvastu", "Kaski", "Kathmandu", "Kavrepalanchok", "Khotang", "Lalitpur", "Lamjung", "Mahottari", "Makwanpur",
+            "Manang", "Morang", "Mugu", "Mustang", "Myagdi", "Nawalparasi", "Nuwakot", "Okhaldhunga", "Palpa", "Panchthar",
+            "Parbat", "Parsa", "Pyuthan", "Ramechhap", "Rasuwa", "Rautahat", "Rolpa", "Rukum", "Rupandehi", "Salyan", "Sankhuwasabha",
+            "Saptari", "Sarlahi", "Sindhuli", "Sindhupalchok", "Siraha", "Solukhumbu", "Sunsari", "Surkhet", "Syangja", "Tanahu",
+            "Taplejung", "Terhathum", "Udayapur"};
+
     public final static String [] AGE_GROUP = {"below 16", "16-18", "19-24", "25-30", "31-35", "36-40", "41-50", "51-60","above 60"};
 }
