0ebb012a89e78bc41b706cafc3331d522d40b9e0
Samir Dangal <smdangal201@gmail.com>
2018-03-14 15:19:43 +0545

    tapItStopIt grid ma top maargin add gareko

---
+++ app/src/main/java/com/nepal/naxa/smartnaari/mycircle/MyCircleProtectorActivity.java
@@ -0,0 +1,15 @@
+package com.nepal.naxa.smartnaari.mycircle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nepal.naxa.smartnaari.R;
+
+public class MyCircleProtectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_circle_protector);
+    }
+}
