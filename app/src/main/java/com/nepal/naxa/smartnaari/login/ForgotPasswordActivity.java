a961b4fbcc48e980403aba8054e3a8d57eed42d9
Samir Dangal <smdangal201@gmail.com>
2018-04-29 11:20:18 +0545

    PaswordProtected MyCircle ma text update gareko

---
+++ app/src/main/java/com/nepal/naxa/smartnaari/login/ForgotPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.nepal.naxa.smartnaari.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nepal.naxa.smartnaari.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
